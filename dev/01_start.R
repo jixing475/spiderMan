9628223bed91a1474d1856d880fc08b2ec302071
jixing <jixing475@163.com>
2021-06-04 08:44:31 +0800

    repo init

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ## 
 golem::fill_desc(
   pkg_name = "spiderMan", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "Some crawling analysis", # The Title of the package containing the App 
+  pkg_description = "Some crawling analysis.", # The Description of the package containing the App 
+  author_first_name = "Jixing", # Your First Name
+  author_last_name = "Liu", # Your Last Name
+  author_email = "jixing475@163.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
@@ -33,7 +33,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license( "Golem User" )  # You can set another license here
+usethis::use_mit_license( "Jixing Liu" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
